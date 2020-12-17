682c6407d18f2c5555b25e8c218f5be652749d83
aniesanies <72492974+aniesanies@users.noreply.github.com>
2020-12-17 19:52:23 +0900

    レイアウト,path,ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create 
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
